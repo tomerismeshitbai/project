717a0d2f32118d522a97699f1ecadd253cc0af35
AntuanRoKantenn <91137160+AntuanRoKantenn@users.noreply.github.com>
2023-05-02 14:41:04 +0600

    Delete src/back/hundred_back directory

--- src/back/hundred_back/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Book, Genre, Cart, Discount, CartItem
-
-admin.site.register(Book)
-admin.site.register(Genre)
-admin.site.register(Cart)
-admin.site.register(Discount)
-admin.site.register(CartItem)
